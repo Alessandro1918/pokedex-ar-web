fc9f9913c9acaca4780e9129df11b449a0d9c6ca
Alessandro1918 <alessandro1918@hotmail.com>
2025-07-04 23:59:49 -0300

    refactor: setup prediction data from back

--- back/index.js
+++ back/index.js
@@ -4,33 +4,31 @@ import * as tf from '@tensorflow/tfjs-node'
 import path from 'path'
 import fs from 'fs'
 import cors from 'cors'
+import 'dotenv/config'
 
 const app = express()
 const port = 5001
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONT_URL,
   optionsSuccessStatus: 200
 }))
 
 // Set up multer for image upload
 const upload = multer({ dest: 'uploads/' })
 
-// Load the model once on startup
-const modelPath = path.resolve('./model/model.json')
-let model
-(async () => {
-  console.log('Loading model...')
-  model = await tf.loadLayersModel(`file://${modelPath}`)
-  console.log('Model loaded')
-})()
-
 app.post('/api/eval', upload.single('file'), async (req, res) => {
-  if (!req.file || !model) {
-    return res.status(400).json({ error: 'No file or model not loaded' })
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file' })
   }
 
   try {
+    const labels = fs.readFileSync("./model/labels.txt", "utf8").split("\n")
+
+    // Load the model
+    const modelPath = path.resolve('./model/model.json')
+    const model = await tf.loadLayersModel(`file://${modelPath}`)
+
     // Read uploaded image
     const imageBuffer = fs.readFileSync(req.file.path)
     const tensor = tf.node
@@ -42,11 +40,18 @@ app.post('/api/eval', upload.single('file'), async (req, res) => {
 
     const prediction = model.predict(tensor)
     const result = await prediction.data()
+    const predictions = Array.from(result)
+    const index = predictions.indexOf(Math.max(...predictions)) // 103
+    const label = labels[index]                                 // Pikachu
+    const accuracy = predictions[index].toFixed(4)              // 0.5142
 
     // Clean up uploaded file
     fs.unlinkSync(req.file.path)
 
-    res.json({ prediction: Array.from(result) })
+    res.json({ 
+      // predictions,
+      prediction: { label, accuracy } 
+    })
   } catch (err) {
     console.error('Inference error:', err)
     res.status(500).json({ error: err.message })
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -2,10 +2,16 @@
 import { useState, useRef } from "react"
 import axios from "axios"
 
+type PredictionProps = {
+  label: string,
+  accuracy: number
+}
+
 export default function Home() {
 
   const inputRef = useRef<HTMLInputElement>(null)
   const [ selectedFile, setSelectedFile ] = useState<string>()
+  const [ prediction, setPrediction ] = useState<PredictionProps>({label: "", accuracy: 0})
 
   function handleFileChange(e: any) {
     setSelectedFile(URL.createObjectURL(e.target.files[0]))
@@ -15,7 +21,8 @@ export default function Home() {
     const form = new FormData()
     const file = inputRef.current?.files ? inputRef.current.files[0] : "-"
     form.append("file", file)
-    axios.post('http://localhost:5001/api/eval', form)
+    const data = axios.post(`${process.env.NEXT_PUBLIC_BACK_URL}/api/eval`, form)
+    setPrediction((await data).data.prediction)
   }
 
   return (
@@ -35,6 +42,11 @@ export default function Home() {
       />
 
       <button onClick={handleSubmit}>Eval</button>
+
+      {
+        prediction.accuracy > 0 &&
+        <h1>{`${prediction.label} (${(100 * prediction.accuracy).toFixed(2)}%)`}</h1>
+      }
     </div>
   )
 }
