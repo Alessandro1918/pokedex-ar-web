50426d484b5f97abc50740afa86e25c2bd1d0e9b
Alessandro1918 <alessandro1918@hotmail.com>
2025-07-23 13:26:06 -0300

    style: add pokedex border to Details page

--- front/src/app/history/page.tsx
+++ front/src/app/history/page.tsx
@@ -19,33 +19,45 @@ export default function History() {
   //     return (
   //       <div 
   //         key={e.name}
-  //         className="flex flex-row items-center gap-2"
+  //         className="flex flex-row items-center justify-center gap-2"
   //       >
   //         <h3 >{e.name}</h3>
   //         <h3 >{e.date}</h3>
   //         <img src={e.image} className="size-8 aspect-auto bg-gray-200"/>
-  //         <button onClick={() => handleDelete(e.name)}>Delete</button>
+  //         {/* <button onClick={() => handleDelete(e.name)}>Delete</button> */}
   //       </div>
   //     )
   //   })
   // )
 
   return (
-    <div className="mt-8 flex flex-col w-screen h-fit items-center justify-center overflow-auto gap-6">
-      <Generation 
-        title="Kanto"
-        names={gen1}
-        startIndex={1}
-        history={history} 
-        setHistory={setHistory} 
-      />
-      <Generation 
-        title="Johto"
-        names={gen2}
-        startIndex={152}
-        history={history} 
-        setHistory={setHistory} 
-      />
+    <div className="p-3 h-screen w-full mx-auto max-w-full sm:max-w-sm md:max-w-md lg:max-w-lg rounded-2xl bg-gradient-to-r from-red-600 via-red-400 to-red-600 border-2 border-red-800">
+      <div className="p-3 pb-8 h-full w-full rounded-md bg-gradient-to-r from-zinc-400 via-zinc-200 to-zinc-400 border-2 border-zinc-600">
+        <div className="p-2 h-full w-full rounded-md space-y-4 overflow-auto bg-blue-600 border-2 border-blue-800">
+          <Generation 
+            title="Kanto"
+            names={gen1}
+            startIndex={1}
+            history={history} 
+            setHistory={setHistory} 
+          />
+          <Generation 
+            title="Johto"
+            names={gen2}
+            startIndex={152}
+            history={history} 
+            setHistory={setHistory} 
+          />
+        </div>
+        <div className="p-2 flex flex-row items-center justify-between">
+          <div className="size-4 bg-red-600 border-2 border-black rounded-full"></div>
+          <div className="flex flex-col gap-1">
+            <div className="w-16 h-0.5 bg-zinc-800 rounded-b-4xl"></div>
+            <div className="w-16 h-0.5 bg-zinc-800 rounded-b-4xl"></div>
+            <div className="w-16 h-0.5 bg-zinc-800 rounded-b-4xl"></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
